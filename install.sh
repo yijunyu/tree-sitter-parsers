2506d39f586f2f4a1ef0b7bc2c1ea1f10b7e56a0
Yijun Yu <y.yu@open.ac.uk>
2021-10-02 11:39:24 +0000

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for l in bash c cpp c-sharp css elm go html java javascript kotlin lua php python ruby rust scala solidity verilog yaml; do 
+for l in bash c cpp c-sharp css elm go haskell html java javascript kotlin lua php python ruby rust scala solidity verilog yaml; do 
     npm pack tree-sitter-$l
 done
 for l in *.tgz; do
